ed123d6d9b49a98ff34ccc36b7ceebf3c1fb871e
uzo-john <johnpauluzowuru2018@gmail.com>
2023-06-22 05:18:58 -0700

    tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""
-    module containing Amenity class
-"""
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
-from models.city import City
-from models.user import User
-from models.place import place_amenity
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
     """
-        Amenity class
-    """
-
-    if (storage_engine == "db"):
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place",
-            secondary=place_amenity, back_populates="amenities")
-    else:
-        name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
